73bacd4488826f6aaee13749e29eb8166e0e2a92
rafaelsousa2099 <rafaelsousa2187@gmail.com>
2019-10-07 01:23:19 -0300

    Edit

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 // Load the TCP Library
 const net = require('net')
+const MessageTypes = require('../shared/messageTypes')
 
-// Keep track of the chat clients
-const clients = []
+let waitList = [] //List of users waiting to play
+let playingList = []
 
 // Start a TCP Server
 net
@@ -10,39 +11,24 @@ net
     // Identify this client
     socket.name = socket.remoteAddress + ':' + socket.remotePort
 
-    // Put this new client in the list
-    clients.push(socket)
-
-    broadcast(socket.name + ' joined the chat\n', socket)
-
-    // Handle incoming messages from clients.
+    // Wait for user messages
     socket.on('data', data => {
-      console.log('> ' + data.toLocaleString())
-      setTimeout(() => {
-        socket.write(data.toLocaleString())
-      }, 1000)
-      // socket.pipe(socket)
-      // broadcast(socket.name + '> ' + data, socket)
+      let aux = JSON.parse(data)
+
+      if (aux.type === MessageTypes.newUser) {
+        // When the User create the nickname -> add User in the waitList
+        user = {
+          nickname: data.nickname,
+          socket
+        }
+
+        waitList.push(user)
+      }
     })
 
     // Remove the client from the list when it leaves
     socket.on('end', function() {
-      clients.splice(clients.indexOf(socket), 1)
-      broadcast('\n' + socket.name + ' left the chat.\n')
+      waitList.splice(waitList.indexOf(socket), 1)
     })
-
-    // Send a message to all clients
-    function broadcast(message, sender) {
-      clients.forEach(client => {
-        // Don't want to send it to sender
-        if (client === sender) return
-        client.write(message)
-      })
-      // Log it to the server output too
-      process.stdout.write(message + '\n')
-    }
   })
   .listen(5000)
-
-// Put a friendly message on the terminal of the server.
-console.log('Chat server running at port 5000\n')
