a114ebb4f1acf5f3f7e647c1e2d59af1f3739e91
Rafael Nascimento de Sousa <rafaelsousa2187@gmail.com>
2019-10-10 13:35:32 -0300

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 // Load the TCP Library
-const net = require('net')
+const net = require('net') // net module -: provides an asynchronous network API
 const MessageTypes = require('../shared/messageTypes')
 
 let waitList = [] //List of users waiting to play
@@ -8,21 +8,54 @@ let playingList = []
 // Start a TCP Server
 net
   .createServer(socket => {
+    socket.setEncoding('utf8') //change the recieved type of data from buffer to string
+
     // Identify this client
     socket.name = socket.remoteAddress + ':' + socket.remotePort
+    console.log(socket.name)
 
     // Wait for user messages
     socket.on('data', data => {
-      let aux = JSON.parse(data)
+      let aux = JSON.parse(data) //convert string(JSON) to obj
 
-      if (aux.type === MessageTypes.newUser) {
-        // When the User create the nickname -> add User in the waitList
-        user = {
-          nickname: data.nickname,
-          socket
-        }
-
-        waitList.push(user)
+      switch (aux.type) {
+        case MessageTypes.newUser:
+          // check if list is empty
+          if (waitList.length) {
+            for (i = 0; i < waitList.length; i++) {
+              if (!waitList[i].nickname == data.nickname) {
+                // When the User create the nickname -> add Object user in the waitList
+                user = {
+                  nickname: data.nickname,
+                  socket
+                }
+                waitList.push(user)
+                // mensagem de resposta ao cliente
+                socket.write('Accepted')
+                break
+              } else {
+                socket.write('Denied')
+              }
+            }
+          } else {
+            // When the User create the nickname -> add Object user in the waitList
+            user = {
+              nickname: data.nickname,
+              socket
+            }
+            waitList.push(user)
+            // adicionar mensagem de resposta ao cliente
+            socket.write('Accepted')
+          }
+          break
+        case MessageTypes.move:
+          break
+        case MessageTypes.start:
+          break
+        case MessageTypes.err:
+          break
+        default:
+          break
       }
     })
 
