19e1595c3e4c9c6c851e8978378d930cab613aa6
Caio Shimada Rabello <xcaiosr@gmail.com>
2019-10-09 22:40:10 -0300

    fixed asyncs and typos

--- src/addUser.js
+++ src/addUser.js
@@ -1,14 +1,11 @@
-function addNewUser() {
+async function addNewUser() {
   const Requestor = request('./utils/requestor')
   const MessageTypes = request('../shared/messageTypes')
 
   let req = new Requestor()
-  ;(async () => {
-    const res = await req.request({
-      type: MessageTypes.newUser,
-      value: document.getElementById('nick-field').value
-    })
-    console.log(res)
-    document.getElementById('nick-form').innerHTML += res
-  })()
+  const res = await req.request({
+    type: MessageTypes.newUser,
+    value: document.getElementById('nick-field').value
+  })
+  console.log(res)
 }
--- src/requestPlayers.js
+++ src/requestPlayers.js
@@ -1,19 +1,17 @@
 const Requestor = request('./utils/requestor')
 const MessageTypes = request('../shared/messageTypes')
 
-function getUsersList() {
+async function getUsersList() {
   let req = new Requestor()
-  ;(async () => {
-    const res = await req.request({
-      type: MessageTypes.listUsers
-    })
-    return res
-  })()
+  const res = await req.request({
+    type: MessageTypes.listUsers
+  })
+  return res
 }
 
 function populateUserList() {
   users = getUsersList()
 
   document.getElementById('players-table-id').innerHTML +=
-    '<tr><td>' + users + "</td><td><a href='game.html'>Challenge<a/><td/></tr>"
+    '<tr><td>' + users + "</td><td><a href='game.html'>Challenge</a></td></tr>"
 }
